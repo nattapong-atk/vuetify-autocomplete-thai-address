58127985097e80d1271d2259db417d6deeaa7a22
nattapong-atk <nattapong.atk@gmail.com>
2021-05-29 15:00:20 +0700

    set config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,59 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    es6: true,
+    node: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
+    'plugin:vue/recommended',
+    '@vue/standard',
+    'airbnb-base'
   ],
+  rules: {
+    'max-len': ['error', {
+      code: 150,
+      tabWidth: 2,
+      ignoreUrls: true,
+      ignoreRegExpLiterals: true,
+      ignoreComments: true,
+      ignoreTrailingComments: true,
+      ignoreStrings: true
+    }],
+    'radix': ['error', 'as-needed'],
+    'template-curly-spacing': 'off',
+    'indent': 'off', // dynamic require
+    'no-plusplus': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? ['error', { allow: ['warn', 'error'] }] : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? ['error'] : 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/component-name-in-template-casing': 'off',
+    'comma-dangle': ['error', 'never'],
+    'semi': ['error', 'never'],
+    'quote-props': ['error', 'consistent-as-needed'],
+    'import/no-unresolved': 'off',
+    'space-before-function-paren': ['error', 'always'],
+    'object-curly-newline': 'off',
+    'no-underscore-dangle': 'off',
+    'no-shadow': ['error', { allow: ['state'] }],
+    'prefer-destructuring': ['error', { object: true, array: false }],
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'acc', // for reduce accumulators
+        'state',
+        'el',
+        'Vue'
+      ]
+    }],
+    'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+    'no-bitwise': ['error', { int32Hint: true }]
+  },
   parserOptions: {
     parser: 'babel-eslint',
+    allowImportExportEverywhere: true
   },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-};
+  settings: {
+    'import/core-modules': ['vue', 'vuex'] // these modules are included in nuxt.js
+  }
+}
